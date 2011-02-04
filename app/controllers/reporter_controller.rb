cceb3e0ea5e2dbe98c616eee27eb6f2c28c0dd41
Kurt Christensen <kurt.j.christensen@gmail.com>
2011-02-04 04:34:39 -0600

    Updated copyrights

--- app/controllers/reporter_controller.rb
+++ app/controllers/reporter_controller.rb
@@ -1,5 +1,16 @@
-# Much of this was originally written in Java, and probably violates the Ruby way.
-# Readers are encouraged to improve it!
+#
+# Copyright (c) Kurt Christensen, The Bit Bakery, 2011.
+#
+# Licensed under the Artistic License, Version 2.0 (the "License"); you may not use this
+# file except in compliance with the License. You may obtain a copy of the License at:
+#
+# http://www.opensource.org/licenses/artistic-license-2.0.php
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+#
 
 require 'reporter_utility'
 
@@ -11,8 +22,6 @@ require 'reporter_utility'
 # View: Should add parameters to filter on particular parameters
 # Controller: Need to figure out if its possible to parameterize a block string in Ruby - I'd like to do this to my big SQL string
 #
-
-
 class ReporterController < ApplicationController
   unloadable
 
